a47cf5daff9b7eaf8ae2ff4d1ffb10914ce5c294
Alicia Hallal <S3811836@student.rmit.edu.au>
2021-01-16 20:00:29 +1100

    Added billpay, payee and transaction models with validation. Initial trello image added.

---
+++ A2/Models/BillPay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace A2.Models
+{
+    public enum Period
+    {
+        Monthly = 'M',
+        Quarterly = 'Q',
+        OnceOff = 'S'
+    };
+    public class BillPay
+    {
+        [Required]
+        public int BillPayID { get; set; }
+        [Required]
+        [ForeignKey("Account")]
+        public int AccountNumber { get; set; }
+        public virtual Account Account { get; set; }
+        [Required]
+        [ForeignKey("PayeeID")]
+        public int PayeeID { get; set; }
+        public virtual Payee PayeeIDNo { get; set; }
+        [Required]
+        public DateTime ScheduleDate { get; set; }
+        [Required]
+        [StringLength(1)]
+        public Period Period { get; set; }
+        [Required]
+        public DateTime ModifyDate { get; set; }
+    }
+}
---
+++ A2/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2.Models
+{
+    public enum TransactionType
+    {
+        Deposit = 'D',
+        Withdraw = 'W',
+        Transfer = 'T',
+        ServiceCharge = 'S',
+        BillPay = 'B'
+    };
+    public class Transaction
+    {
+        [Required]
+        public int TransactionID { get; set; }
+        [Required]
+        [StringLength(1)]
+        public TransactionType TransactionType { get; set; }
+        [Required]
+        [ForeignKey("Account")]
+        public int AccountNumber { get; set; }
+        public virtual Account Account { get; set; }
+        [ForeignKey("DestinationAccount")]
+        public int? DestAccount { get; set; }
+        public virtual Account DestAccount { get; set; }
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+        [StringLength(255)]
+        public string Comment { get; set; }
+        public DateTime ModifyDate { get; set; }
+    }
+}
