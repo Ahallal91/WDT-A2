34565fabdb3c781432ae780184f59c28d2d5c891
Alicia Hallal <S3811836@student.rmit.edu.au>
2021-01-22 09:38:43 +1100

    fixed issue with library not being inside project folder.

---
+++ Validation/GetJson.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Utilities
+{
+    public class GetJson
+    {
+        /// <summary>
+        /// GetJsonByURLAsync retrieves a JSON data by URL and returns the data as a List of specified Objects.
+        /// </summary>
+        /// <param name="URL">The Json URL to contact.</param>
+        /// <param name="dateFormat">If there is need to format the DateFormatString of returned Json, enter dateformat here.</param>
+        /// <exception cref="HttpRequestException"></exception>
+        public static async Task<List<T>> GetJsonByURLAsync<T>(string URL, string dateFormat = "")
+        {
+            using var client = new HttpClient();
+            var json = "";
+            try
+            {
+
+                HttpResponseMessage response = await client.GetAsync(URL);
+                response.EnsureSuccessStatusCode();
+                json = response.Content.ReadAsStringAsync().Result;
+                await Task.FromResult(json);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return JsonConvert.DeserializeObject<List<T>>(json, new JsonSerializerSettings
+            {
+                DateFormatString = dateFormat
+            });
+        }
+    }
+}
---
+++ Validation/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Validator class contains static methods for general validation.
+    /// </summary>
+    public class Validator
+    {
+        /// <summary>
+        /// FourDigits validates 4 digit integers
+        /// </summary>
+        /// <param name="value">integer to validate</param>
+        /// <returns>True if validation is successful. Otherwise false.</returns>
+        public static bool FourDigits(int value)
+        {
+            Regex Rgx = new Regex(@"\d{4}");
+            return Rgx.IsMatch(value.ToString());
+        }
+        /// <summary>
+        /// EightDigits validates 8 digit strings
+        /// </summary>
+        /// <param name="value">string to validate</param>
+        /// <returns>True if validation is successful. Otherwise false.</returns>
+        public static bool EightDigits(string value)
+        {
+            Regex Rgx = new Regex(@"\d{4}");
+            return Rgx.IsMatch(value);
+        }
+        /// <summary>
+        /// NullEmptyValue validates strings
+        /// </summary>
+        /// <param name="value">integer to validate</param>
+        /// <returns>True if string is null or empty, otherwise false.</returns>
+        public static bool NullEmptyValue(string value)
+        {
+            return (value == null) || (value == "");
+        }
+    }
+}
