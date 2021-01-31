bc3761fefb03c4d3e861021d94eab204deda47ba
Alicia Hallal <S3811836@student.rmit.edu.au>
2021-01-31 18:24:37 +1100

    documented loginmanager

--- WebAPI/Model/Manager/LoginManager.cs
+++ WebAPI/Model/Manager/LoginManager.cs
@@ -14,23 +14,36 @@ namespace WebAPI.Model.Manager
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Allows Admin project to get all user logins available in the database
+        /// and returns them as a list.
+        /// </summary>
+        /// <returns>A List of AspNetUsers (logins) </returns>
         public List<AspNetUser> GetAll()
         {
             return _context.AspNetUsers.ToList();
         }
-
+        /// <summary>
+        /// Allows Admin project to get all user logins by their id and return them as a list.
+        /// </summary>
+        /// <returns>A List of AspNetUsers (logins) that match the id</returns>
         public List<AspNetUser> GetAllByID(int id)
         {
 
             return _context.AspNetUsers.Where(x => x.Id == id.ToString()).ToList();
         }
-
+        /// <summary>
+        /// Currently not implemented in admin project so not implemented here.
+        /// </summary>
         public List<AspNetUser> GetAllByIDWithDate(int id, DateTime startDate, DateTime endDate)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Allows Admin to update a login user to
+        /// lock/unlock user logins and update the lockout in the database.
+        /// </summary>
+        /// <returns>id that was locked or unlocked</returns>
         public int Update(int id, AspNetUser login)
         {
             _context.Update(login);
